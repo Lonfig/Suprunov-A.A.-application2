799b23d4ada7eb0c31642b4469a900500e06b5e4
Lonfig <suprunov250302@gmail.com>
2020-10-23 01:16:20 +0700

    3 'экрана'

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,13 +2,43 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private Button button;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.buttonTo1From2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.buttonTo3From2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
     }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -1,22 +1,22 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     private Button button;
     private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
 
-        button = (Button) findViewById(R.id.buttonTo2);
+        button = (Button) findViewById(R.id.buttonTo1From3);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button2 = (Button) findViewById(R.id.buttonTo3From1);
+        button2 = (Button) findViewById(R.id.buttonTo2From3);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -33,12 +33,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void openActivity2(){
-        Intent intent = new Intent(this, MainActivity2.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
     public void openActivity3(){
-        Intent intent = new Intent(this, MainActivity3.class);
+        Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
-
 }
